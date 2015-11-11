5a606caf986704df94d03361d3f65eeaa1c74ecb
Dittaphong Nilnama <aostudio.nilnama@gmail.com>
2015-11-11 16:57:55 +0700

    แก้  Part File

--- application/views/header.php
+++ application/views/header.php
@@ -1,18 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-
+<html lang="en"> 
+<head> 
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<title>Bare - Start Bootstrap Template</title>
-
+	<meta name="author" content=""> 
+	<title>Bare - Start Bootstrap Template</title> 
 	<!-- Bootstrap Core CSS -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>css/bootstrap.min.css" rel="stylesheet">
 
 	<!-- Custom CSS -->
 	<style>
